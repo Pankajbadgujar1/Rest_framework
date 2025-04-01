4d2351c11ea1b6005b0b6597772c4c5350c9cd0e
Pankaj kumar <pankaj14badgujar@gmail.com>
2025-04-01 16:41:10 +0530

    custome validation

--- home/serializers.py
+++ home/serializers.py
@@ -11,3 +11,15 @@ class StudentSerializer(serializers.ModelSerializer):
           
         fields = '__all__'
         
+    def validate(self, data):
+        if data['name']:
+            for n in data['name']:
+                if n.isdigit():
+                    raise serializers.ValidationError({'error', 'name cannot be numeric'})
+                
+
+        if data['age'] < 18:
+            raise serializers.ValidationError({'error': 'age cannot be less than  18'})
+        
+
+        return data
